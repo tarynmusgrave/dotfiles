6cd558655704baba5dd673b8d3fc86ff5b715f6f
Taryn Musgrave <34845739+tarynmusgrave@users.noreply.github.com>
2021-04-20 11:17:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Update the git config
-cp gitconfig ~/.gitconfig
+cp .gitconfig ~/.gitconfig
 
 cp tmux.conf ~/.tmux.conf
-cp zshrc ~/.zshrc
+cp .zshrc ~/.zshrc
 
 tmux source-file ~/.tmux.conf
